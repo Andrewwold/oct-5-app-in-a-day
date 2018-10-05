79bd794d4aa4715779fabc9e088fe4fd8e9ef56e
Joe Anderson <joseph.anderson423@gmail.com>
2018-10-05 16:19:59 -0600

    finished fixing styles

--- src/components/signIn/signInForm.js
+++ src/components/signIn/signInForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
 class SignInForm extends Component {
@@ -7,8 +8,8 @@ class SignInForm extends Component {
         const { handleSubmit } = this.props;
         return (
             <form onSubmit={ handleSubmit } className='signIn-form'>
-                <Field name='email' type='email' component='input' />
-                <Field name='password' type='password' component='input' />
+                <Field className="input" name='email' type='email' component='input' />
+                <Field className="input" name='password' type='password' component='input' />
                 <button className='submitButton'>Submit</button>
             </form>
         );
@@ -18,5 +19,13 @@ class SignInForm extends Component {
 SignInForm = reduxForm({
     form: 'SignInForm'
 })(SignInForm);
+
+function mapStateToProps(state) {
+    return { 
+        initialValues: { email: 'example@gmail.com', password: '' }
+    }
+}
+
+SignInForm = connect(mapStateToProps)(SignInForm);
 
 export default SignInForm;
