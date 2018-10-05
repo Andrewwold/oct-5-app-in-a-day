d7662f87c1234acd25da55a4dc0f03f1b711f30c
Brodie Kelley <brodiekelley@hotmail.com>
2018-10-05 14:07:42 -0600

    Cleaned up some code

--- src/components/signInForm.js
+++ src/components/signInForm.js
@@ -3,16 +3,16 @@ import { Field, reduxForm } from 'redux-form';
 
 class SignInForm extends Component {
 
-  render() {
-      const { handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit} className='signIn-form'>
-        <Field name='email' type='email' component='input'/>
-        <Field name='password' type='password' component='input'/>
-        <button className='submitButton'>Submit</button>
-      </form>
-    );
-  }
+    render() {
+        const { handleSubmit } = this.props;
+        return (
+            <form onSubmit={ handleSubmit } className='signIn-form'>
+                <Field name='email' type='email' component='input' />
+                <Field name='password' type='password' component='input' />
+                <button className='submitButton'>Submit</button>
+            </form>
+        );
+    }
 }
 
 SignInForm = reduxForm({
